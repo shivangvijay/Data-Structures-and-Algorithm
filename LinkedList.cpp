f39dbbcc1535650b207e6d5c9971d24217f1ddbf
Shivang Vijay <shivangvijay@gmail.com>
2022-07-18 03:23:29 +0530

    added insertEnd and insertBegin

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -33,9 +33,38 @@ void recTraverse(Node *head)
     recTraverse(node->next);
 }
 
+Node *insertBegin(Node *head, int x)
+{
+    Node *newHead = new Node(x);
+    newHead->next = head;
+    return newHead;
+}
+
+// need to make some changes
+Node *insertEnd(Node *head, int x)
+{
+    Node *node = head;
+    if(node == NULL)
+    {
+        node = new Node(x);
+        return node;
+    }
+    while(node->next != NULL)
+    {
+        node = node->next;
+    }
+    node->next = new Node(x);
+    return head;
+}
+
 int main(){
     Node *head = new Node(10);
     head->next = new Node(11);
     head->next->next = new Node(12);
-    recTraverse(head);
+    head = insertBegin(head,5);
+    head = insertBegin(head,19);
+    head = insertEnd(head,99);
+    Node *head2 = NULL;
+    head2 = insertEnd(head2,189);
+    traverse(head);
 }
